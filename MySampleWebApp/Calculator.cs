d249919eefc8d067b8be3d7b2ddf5f39ea5216c8
Umarani Tejomurtula <umarani@hitechcto.onmicrosoft.com>
2021-08-06 17:03:47 +0530

    added unit tests

---
+++ MySampleWebApp/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySampleWebApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return b - a;
+        }
+    }
+}
---
+++ MySampleWebAppTests1/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySampleWebApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySampleWebApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calc = new Calculator();
+            int actualtResult = calc.Add(2, 3);
+            Assert.AreEqual(actualtResult, 5);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Calculator calc = new Calculator();
+            int actualtResult = calc.Subtract(3, 6);
+            Assert.AreEqual(actualtResult, 3);
+        }
+    }
+}
